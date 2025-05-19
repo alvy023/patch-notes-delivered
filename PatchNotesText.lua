ee640ba5b20fd66b0ba9cd60f3a3cbec295d46ad
Alex <aralvarez023@gmail.com>
2025-05-19 11:50:02 -0400

    Create PatchNotesText.lua

---
+++ PatchNotesText.lua
@@ -0,0 +1,13 @@
+-- Patch Notes Delivered Addon
+-- Author: alvy023
+-- File: PatchNotesText.lua
+-- Description: Patch notes text file for the PND addon.
+-- License: NA
+-- For more information, visit the project repository.
+
+-- Initialize the global patch notes variable
+PatchNotesDelivered_Text = [[
+|cffffd200Patch Notes Delivered - v0.1.0|r
+• Displays patch notes once per addon version
+• Addon updated with the latest patch notes
+]]
