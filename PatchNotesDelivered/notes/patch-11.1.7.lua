a4f3b21060311d4e094e13530f92d334f7a19a26
Alex <aralvarez023@gmail.com>
2025-06-17 11:38:41 -0400

    Create patch-11.1.7.lua

---
+++ PatchNotesDelivered/notes/patch-11.1.7.lua
@@ -0,0 +1,32 @@
+-- Patch Notes Delivered Addon
+-- Author: alvy023
+-- File: patch-11.1.7.lua
+-- Description: Patch notes text file for patch 11.1.7
+-- License: License.txt
+-- For more information, visit the project repository.
+
+--- Export global notes variable
+PatchNotesDelivered_Notes_1117 = {
+    version = "11.1.7",
+    build = "61491",
+    hotfix = 0,
+    gameChangesHotfixes = [[
+        ]],
+
+        gameChangesPatch = [[
+        ]],
+
+        addonChanges = [[
+            May 29, 2025
+
+                • First Patch Notes Delivered (PND) release!
+                • Displays official WoW patch notes and addon updates on login
+                • Adds a minimap button and Addon Compartment entry for quick access
+                    - Left-click to show patch notes
+                    - Right-click for options menu (hide minimap button, hide Addon Compartment entry)
+                • Slash commands:
+                    - "/pnd" – Show patch notes
+                    - "/pnd-mini" – Toggle minimap button
+        ]],
+    }
+        
