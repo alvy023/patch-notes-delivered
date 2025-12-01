ea36a299fede33a7a5214a62eebec7be8d76ffdc
Alex <aralvarez023@gmail.com>
2025-12-01 19:53:21 +0000

    Update to Patch 11.2.7

---
+++ PatchNotesDelivered/notes/patch-11.2.7.lua
@@ -0,0 +1,20 @@
+-- Patch Notes Delivered Addon
+-- Author: alvy023
+-- File: patch-11.2.7.lua
+-- Description: Patch notes text file for patch 11.2.7
+-- License: License.txt
+-- For more information, visit the project repository.
+
+--- Export global notes variable
+--- Max note length [140] ------------------------------------------------------------------------------------------------------------------
+PatchNotesDelivered_Notes_1127 = {
+    version = "11.2.7",
+    build = "00000",
+    hotfix = 0,
+    gameChangesHotfixes = [[
+    ]],
+    gameChangesPatch = [[
+    ]],
+    addonChanges = [[
+    ]],
+}
