b6572b4f5cd80ebb9fe44670ce4dce18a77c419f
Alex <aralvarez023@gmail.com>
2026-01-15 19:52:46 +0000

    Add Midnight Pre-Patch notes

---
+++ PatchNotesDelivered/notes/patch-11.2.7-pre.lua
@@ -0,0 +1,46 @@
+-- Patch Notes Delivered Addon
+-- Author: alvy023
+-- File: patch-11.2.7-pre.lua
+-- Description: Patch notes text file for patch 11.2.7
+-- License: License.txt
+-- For more information, visit the project repository.
+
+--- Export global notes variable
+--- Max note length [140] ------------------------------------------------------------------------------------------------------------------
+PatchNotesDelivered_Notes_1127P = {
+    version = "11.2.7P",
+    build = "65299",
+    hotfix = 0,
+    gameChangesHotfixes = [[
+    ]],
+    gameChangesPatch = [[
+    ]],
+    deathKnightChangesPatch = [[
+    ]],
+    demonHunterChangesPatch = [[
+    ]],
+    druidChangesPatch = [[
+    ]],
+    evokerChangesPatch = [[
+    ]],
+    hunterChangesPatch = [[
+    ]],
+    mageChangesPatch = [[
+    ]],
+    monkChangesPatch = [[
+    ]],
+    paladinChangesPatch = [[
+    ]],
+    priestChangesPatch = [[
+    ]],
+    rogueChangesPatch = [[
+    ]],
+    shamanChangesPatch = [[
+    ]],
+    warlockChangesPatch = [[
+    ]],
+    warriorChangesPatch = [[
+    ]],
+    addonChanges = [[
+    ]],
+}
