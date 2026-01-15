d681121bb0ae554052a61ce2477f507c2ffd3d84
Alex <aralvarez023@gmail.com>
2026-01-15 18:26:35 -0500

    Fix patch version in notes

--- PatchNotesDelivered/notes/patch-11.2.7-pre.lua
+++ PatchNotesDelivered/notes/patch-11.2.7-pre.lua
@@ -8,7 +8,7 @@
 --- Export global notes variable
 --- Max note length [140] ------------------------------------------------------------------------------------------------------------------
 PatchNotesDelivered_Notes_1127P = {
-    version = "11.2.7 Pre-Expansion",
+    version = "11.2.7P",
     build = "65299",
     hotfix = 0,
     gameChangesHotfixes = [[
